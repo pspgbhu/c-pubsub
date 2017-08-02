6c6d42b2ff798b83e404807bdc5bd456847e053b
hucong <hucong@jd.com>
2017-08-02 09:51:26 +0800

    Ues babel

--- dist/pubsub.js
+++ dist/pubsub.js
@@ -10,32 +10,29 @@
  * @Reference http://taobaofed.org/blog/2016/11/17/react-components-communication/
  */
 
-const pubsub = {
+var pubsub = {
   onObj: {},
   oneObj: {},
 
-  on(key, fn) {
+  on: function on(key, fn) {
     if (this.onObj[key] === undefined) {
       this.onObj[key] = [];
     }
     this.onObj[key].push(fn);
   },
-
-  one(key, fn) {
+  one: function one(key, fn) {
     if (this.oneObj[key] === undefined) {
       this.oneObj[key] = [];
     }
     this.oneObj[key].push(fn);
   },
-
-  off(key) {
+  off: function off(key) {
     this.onObj[key] = [];
     this.oneObj[key] = [];
   },
-
-  emit() {
-    let key;
-    let args;
+  emit: function emit() {
+    var key = void 0;
+    var args = void 0;
     if (arguments.length === 0) {
       return false;
     }
@@ -43,20 +40,20 @@ const pubsub = {
     args = [].concat(Array.prototype.slice.call(arguments, 1));
 
     if (this.onObj[key] !== undefined && this.onObj[key].length > 0) {
-      for (let i in this.onObj[key]) {
+      for (var i in this.onObj[key]) {
         this.onObj[key][i].apply(null, args);
       }
     }
 
     if (this.oneObj[key] !== undefined && this.oneObj[key].length > 0) {
-      for (let i in this.oneObj[key]) {
-        this.oneObj[key][i].apply(null, args);
-        this.oneObj[key][i] = undefined;
+      for (var _i in this.oneObj[key]) {
+        this.oneObj[key][_i].apply(null, args);
+        this.oneObj[key][_i] = undefined;
       }
       this.oneObj[key] = [];
     }
     return true;
-  },
+  }
 };
 
 return pubsub;
--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,11 @@
+const babel = require('rollup-plugin-babel');
+
 module.exports = {
   entry: 'src/pubsub.js',
   format: 'umd',
   moduleName: 'pubsub',
-  dest: 'dist/pubsub.js'
-}
+  dest: 'dist/pubsub.js',
+  plugins: [
+    babel({}),
+  ],
+};
